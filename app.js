a3926bef2c156212072208744d6f889620ba4f18
Bryan <balegria.gonz@gmail.com>
2022-07-10 01:33:03 -0500

    Fetch today weather data

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const pressure = document.querySelector(".pressure");
     fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${keyIP}`)
     .then(res => res.json())
     .then(result => {
-        console.log(result.latitude)
-        console.log(result.longitude)
+        // console.log(result.latitude)
+        // console.log(result.longitude)
         getWeather(result)
     })
     .catch(error => console.log(error));
@@ -26,33 +26,86 @@ const pressure = document.querySelector(".pressure");
 
 
 
-    // Get the location coordinates
+    // Get the API data
     function getWeather(geo) {
 
         const {latitude:lat, longitude:lon, city, country} = geo;
+
+
+        // FETCH TODAY DATA
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${keyWeather}`)
+            .then(res => res.json())
+            .then(currentDayResult => {
+                console.log(currentDayResult)
+                //result destructuring
+                const {
+                    main: {humidity: currentHumidity, pressure: currentPressure, temp: currentTemp},
+                    visibility: currentVisibility,
+                    wind: {speed: currentSpeed},
+                    weather: [
+                        {
+                            icon: currentIcon
+                        }
+                    ]
+                
+                } = currentDayResult;
+
+                const todayInfo = [currentTemp, currentSpeed, currentHumidity, currentVisibility, currentPressure, currentIcon];
+                
+                renderCurrentDay(todayInfo);
+            })
     
-    
+        //FETCH 5 DAYS DATA
         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${keyWeather}`)
                 .then(res => res.json())
-                .then(result => {
+                .then(result5Days => {
                     console.log(`${city}, ${country}`)
-                    console.log(result);
-                    renderWeatherInfo(result)
+                    console.log(result5Days);
+                    renderWeatherInfo(result5Days)
                 })
                 .catch(err => {
                     console.log(err);
                 })
     };
 
-    // Display the weather info
+    // RENDER TODAY INFO
+
+    function renderCurrentDay (todayInfo) {
+        const todayTemp =  Math.floor(todayInfo[0] - 273.15);
+        const todayWind = Math.floor(todayInfo[1] * 2.237); //convert to mph
+        const todayHumidity = todayInfo[2];
+        const todayVisibility = ( todayInfo[3] / 1609).toFixed(1);
+        const todayAirPressure = todayInfo[4];
+
+        // INSERT HTML INTO HIGHLIGHTS CARDS
+        const windValue = document.createElement('p');
+        windValue.classList.add('wind-value');
+        windValue.innerHTML = `${todayWind}<span>mph</span>`
+        wind.appendChild(windValue);
+
+        const humidityValue = document.createElement('p');
+        humidityValue.classList.add('wind-value');
+        humidityValue.innerHTML = `${todayHumidity}<span>%</span>`
+        humidity.appendChild(humidityValue);
+
+        const visibilityValue = document.createElement('p');
+        visibilityValue.classList.add('wind-value');
+        visibilityValue.innerHTML = `${todayVisibility} <span>miles</span>`
+        visibility.appendChild(visibilityValue);
+
+        const airPressureValue = document.createElement('p');
+        airPressureValue.classList.add('wind-value');
+        airPressureValue.innerHTML = `${todayAirPressure} <span>mb</span>`
+        pressure.appendChild(airPressureValue);
+
+
+    }
+
+    // RENDER 5 DAYS INFO
 
     function renderWeatherInfo(weatherInfo) {
 
         for (let i = 2; i < 35; i += 8) {
-            // const visibility = (weatherInfo.list[i].visibility / 1609).toFixed(1); //convert meters to miles
-            // const windSpeed = Math.floor(weatherInfo.list[i].wind.speed * 2.237);
-            // const airPressure = weatherInfo.list[i].main.pressure;
-            // const humidity = weatherInfo.list[i].main.humidity
             // const tempMax = Math.floor(weatherInfo.list[i].main.temp_max - 273.15);
             // const tempMin = Math.floor(weatherInfo.list[i].main.temp_min - 273.15);
             // const currentTemp = Math.floor(weatherInfo.list[i].main.temp - 273.15);
@@ -80,11 +133,6 @@ const pressure = document.querySelector(".pressure");
 
         }
 
-            const windStatus = document.createElement('p');
-            windStatus.classList.add('wind-value');
-            windStatus.innerHTML = `${Math.floor(weatherInfo.list[2].wind.speed * 2.237)}<span>mph</span>`
-            wind.appendChild(windStatus);
-
 
 
         // console.log(`${visibility} miles`);
@@ -99,3 +147,5 @@ const pressure = document.querySelector(".pressure");
         // console.log(`${weekDays[newDate.getDay()]}, ${newDate.getDate()} ${months[newDate.getMonth()]}`);
     
     }
+
+
