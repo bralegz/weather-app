5c8fef44497d31af926967068d2cefdba8a56167
Bryan <balegria.gonz@gmail.com>
2022-07-13 22:13:24 -0500

    Render today's weather info on left container

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 // Selectors 
+const todayIconContainer = document.querySelector(".today-iconContainer");
+const todayInfoContainer = document.querySelector(".today-infoContainer");
 const secondRow = document.querySelector(".second-row");
 const wind = document.querySelector(".wind");
 const humidity = document.querySelector(".humidity");
@@ -20,6 +22,7 @@ const pressure = document.querySelector(".pressure");
         // console.log(result.latitude)
         // console.log(result.longitude)
         getWeather(result)
+        // console.log(result)
     })
     .catch(error => console.log(error));
 
@@ -36,7 +39,7 @@ const pressure = document.querySelector(".pressure");
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${keyWeather}`)
             .then(res => res.json())
             .then(currentDayResult => {
-                // console.log(currentDayResult)
+                console.log(currentDayResult)
                 //result destructuring
                 const {
                     main: {humidity: currentHumidity, pressure: currentPressure, temp: currentTemp},
@@ -44,13 +47,14 @@ const pressure = document.querySelector(".pressure");
                     wind: {speed: currentSpeed},
                     weather: [
                         {
-                            icon: currentIcon
+                            icon: currentIcon,
+                            main: mainWeather
                         }
                     ]
                 
                 } = currentDayResult;
 
-                const todayInfo = [currentTemp, currentSpeed, currentHumidity, currentVisibility, currentPressure, currentIcon];
+                const todayInfo = [currentTemp, currentSpeed, currentHumidity, currentVisibility, currentPressure, currentIcon, mainWeather, city];
                 
                 renderCurrentDay(todayInfo);
             })
@@ -78,6 +82,41 @@ const pressure = document.querySelector(".pressure");
         const todayHumidity = todayInfo[2];
         const todayVisibility = ( todayInfo[3] / 1609).toFixed(1);
         const todayAirPressure = todayInfo[4];
+        const todayIcon = todayInfo[5]
+        const todayMain = todayInfo[6]
+        const currentCity = todayInfo[7]
+        const newDate = new Date();
+
+
+        // Insert today's icon image.
+
+        const imgUrl = `http://openweathermap.org/img/wn/${todayIcon}@2x.png`
+        const iconImg = document.createElement('img');
+        iconImg.src = imgUrl;
+        todayIconContainer.appendChild(iconImg);
+
+        //Insert today's info.
+        const todayTempValue = document.createElement('p');
+        todayTempValue.classList.add('todayTempValue');
+        todayTempValue.innerHTML = `${todayTemp}<span>ºC</span>`;
+        todayInfoContainer.appendChild(todayTempValue);
+
+        const todayMainWeather = document.createElement('p');
+        todayMainWeather.classList.add('today-main-weather');
+        todayMainWeather.innerHTML = `${todayMain}`;
+        todayInfoContainer.appendChild(todayMainWeather)
+
+        const todayDate = document.createElement('p');
+        todayDate.classList.add("today-date");
+        todayDate.innerHTML = `Today - ${weekDays[newDate.getDay()]}, ${newDate.getDate()} ${months[newDate.getMonth()]}`;
+        todayInfoContainer.appendChild(todayDate);
+
+        const todayCity = document.createElement('p');
+        todayCity.classList.add("today-city");
+        todayCity.innerHTML = `<i class="fa fa-map-marker" style="font-size:24px"></i> ${currentCity}`
+        todayInfoContainer.appendChild(todayCity);
+
+
 
         // INSERT HTML INTO HIGHLIGHTS CARDS
         const windValue = document.createElement('p');
@@ -108,7 +147,7 @@ const pressure = document.querySelector(".pressure");
     function render5DaysInfo(dayList) {
         
         const filterDayList  = dayList.filter((element, index) => {
-            return index === 8 || index === 16 || index === 24 || index === 32 || index === 39
+            return index === 0 || index === 8 || index === 16 || index === 24 || index === 32
         });
 
         console.log(filterDayList)
